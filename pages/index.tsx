c4fcbd13fc9c9ee9b6c8725c12fed06f4760b8e1
aboulmane <anassboulmane@gmail.com>
2023-07-23 09:22:55 +0000

    feat: Implement debouncing on search input and refactor API call

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
-import { useRef, useState } from "react"
-import Image from "next/image"
+import { ChangeEvent, useState } from "react"
 import { Inter } from "next/font/google"
-import { Button } from "@/components/ui/button"
+import { debounce } from "lodash"
 import { Input } from "@/components/ui/input"
 import { useToast } from "@/components/ui/use-toast"
 import { fetchImages } from "@/lib/fetch-images"
@@ -9,45 +8,49 @@ import { fetchImages } from "@/lib/fetch-images"
 const inter = Inter({ subsets: ["latin"] })
 
 export default function Home() {
-  const inputRef = useRef<any>()
   const [images, setImages] = useState<string[]>([])
-
   const { toast } = useToast()
+  // event handlers
+  const handleSearchInputChange = debounce(
+    async (e: ChangeEvent<HTMLInputElement>) => {
+      try {
+        const value = e.target.value.trim() || ""
+        if (value.length < 3)
+          return toast({ title: "please provide valid input" })
 
-  const handleSubmit = async (e: any) => {
-    e.preventDefault()
-
-    try {
-      const value = inputRef.current?.value.trim() || ""
-      if (value.length < 3)
-        return toast({ title: "please provide valid input" })
-
-      const _images = await fetchImages(value)
-      setImages(_images)
-    } catch (e) {
-      toast({
-        title: "error",
-        description: "something went wrong check the logs",
-      })
-      console.error(e)
-    }
-  }
+        const _images = await fetchImages(value)
+        setImages(_images)
+      } catch (e) {
+        toast({
+          title: "error",
+          description: "something went wrong check the logs",
+        })
+        console.error(e)
+      }
+    },
+    300
+  )
 
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className} space-y-2`}
     >
-      <div className={`flex w-full flex-col`}>
-        <form className="space-y-2" onSubmit={handleSubmit}>
-          <Input ref={inputRef} name="searchTerm" />
-          <Button size="sm" type="submit">
-            Search
-          </Button>
-        </form>
+      <div className="flex w-full flex-col">
+        <Input
+          name="searchTerm"
+          placeholder="search for images"
+          onChange={handleSearchInputChange}
+        />
       </div>
       <div className="grid grid-cols-3 gap-2">
         {images.map((image) => (
-          <img key={image} src={image} alt="no" className="w-full" />
+          <img
+            key={image}
+            src={image}
+            alt="no"
+            className="w-full"
+            tabIndex={0}
+          />
         ))}
       </div>
     </main>
